cf6b07e7d4b16e1cd9681bca91f469c2bd4cadf5
O-Boat <66618186+O-Boat@users.noreply.github.com>
2022-02-20 23:52:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$Topic = "NodeMCU1" ;			
+			$Topic = "ESP32_1" ;			
 			getMqttfromlineMsg($Topic,$text);   
 			
 		}
